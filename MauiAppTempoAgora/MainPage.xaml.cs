1615bc98af5e10d344c39aeff4fbc3603432f95c
Usuario <Usuario@CPD03-M02>
2024-08-08 15:03:34 -0300

    Continuação

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,120 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Service;
+using System.Diagnostics;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        CancellationTokenSource _cancelTokenSource;
+        bool _isCheckingLocation;
+
+        string? cidade;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _cancelTokenSource = new CancellationTokenSource();
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                GeolocationRequest request =
+                    new GeolocationRequest(GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10));
+
+                Location? location = await Geolocation.Default.GetLocationAsync(
+                    request, _cancelTokenSource.Token);
+
+                if (location != null)
+                {
+                    lbl_latitude.Text = location.Latitude.ToString();
+                    lbl_longitude.Text = location.Longitude.ToString();
+
+                    Debug.WriteLine("-------------------------------------------");
+                    Debug.WriteLine(location);
+                    Debug.WriteLine("-------------------------------------------");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: dispositivo não suporta",
+                    fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: localização desabilitada",
+                    fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: permissão", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro:", ex.Message, "OK");
+            }
+        }
+
+        private async Task<string> GetGeocodeReverseData(
+            double latitude = 47.673988, double longitude = -122.121513)
+        {
+            IEnumerable<Placemark> placemarks =
+                await Geocoding.Default.GetPlacemarksAsync(
+                    latitude, longitude);
+
+            Placemark? placemark = placemarks?.FirstOrDefault();
+
+            Debug.WriteLine("-------------------------------------------");
+            Debug.WriteLine(placemark?.Locality);
+            Debug.WriteLine("-------------------------------------------");
+
+            if (placemark != null)
+            {
+                cidade = placemark.Locality;
+
+                return
+                    $"AdminArea:         {placemark.AdminArea}\n" +
+                    $"CountryCode:       {placemark.CountryCode}\n" +
+                    $"CountryName:       {placemark.CountryName}\n" +
+                    $"FeatureName:       {placemark.FeatureName}\n" +
+                    $"Locality:          {placemark.Locality}\n" +
+                    $"PostalCode:        {placemark.PostalCode}\n" +
+                    $"SubAdminArea:      {placemark.SubAdminArea}\n" +
+                    $"SubLocality:       {placemark.SubLocality}\n" +
+                    $"SubThoroughfare:   {placemark.SubThoroughfare}\n" +
+                    $"Thoroughfare:      {placemark.Thoroughfare}\n";
+            }
+
+            return "Nada";
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            double latitude = Convert.ToDouble(lbl_latitude.Text);
+            double longitude = Convert.ToDouble(lbl_longitude.Text);
+
+            lbl_reverso.Text = await GetGeocodeReverseData(latitude, longitude);
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidade))
+                {
+                    Tempo? previsao = await DataService.GetPrevisaoDoTempo(cidade);
+
+                    string dados_previsao = "";
+                }
+            }
         }
     }
 
